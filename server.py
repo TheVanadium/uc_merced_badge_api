b6457c7838e7e91ef886574402fd45a4bbd9130c
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2024-06-15 11:56:18 -0700

    doc: remove redundant unecessary comments

--- server.py
+++ server.py
@@ -5,25 +5,18 @@ from multiprocessing import Process
 
 app = Flask(__name__)
 
-# badges is the only query string allowed
 @app.route('/courses')
 
 def get_courses():
-    # get badges from query string
     badges = request.args.get('badges')
     badges = badges.split(',')
 
-    # return findCourseOverlap([badge for badge in badges])
-    # return every course in findCourseOverlap, starting with [, and ending with ], double quotes, commas separated
-    # return str(findCourseOverlap([badge for badge in badges])) doesn't work because it gives single quotes
     formattedList = "["
     formattedListCall = sorted(findCourseOverlap([badge for badge in badges]))
     for course in formattedListCall:
-        # if the course has double quotes, remove them
         if "\"" in course:
             course = course.replace("\"", "")
 
-    # now that the course is formatted, add it to the formattedList
     for course in formattedListCall:
         formattedList += "\"" + course + "\", "
 
@@ -36,8 +29,6 @@ def get_courses():
 
 @app.route('/update-date')
 def get_update_date():
-    # get the update date from the file lastScraped.txt
-    # example date format 2023-08-30 16:02:55.051360
     with open("lastScraped.txt") as f:
         updateDate = f.read()
     return updateDate
@@ -46,5 +37,3 @@ if __name__ == '__main__':
     p = Process(target=intervalUpdate)
     p.start()
     app.run(debug=True)
-
-# Path: find_overlap.py
